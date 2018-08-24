cc7470a8ef1e93623c277bac3baa94a9da075559
jallen <jallen@snow.edu>
2018-08-24 09:27:46 -0600

    initial commit

--- Chapter2/MainWindow.xaml.cs
+++ Chapter2/MainWindow.xaml.cs
@@ -23,6 +23,13 @@ namespace Chapter2
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtTypeConverterExample.Text = "This was set by code";
+            txtTypeConverterExample.Background = System.Windows.Media.Brushes.Yellow;
         }
     }
 }
---
+++ Chapter2/SampleDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    public class SampleDataContext
+    {
+        public decimal NumberValue => 12345.67890M;
+    } 
+}
